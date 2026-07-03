45ff9f40b3274ac587fc1a9610926b01c1627c58
Jacob Syser <jaythemanchs@gmail.com>
2026-07-03 23:54:48 +1000

    Mock UI with some functionality

--- js/index.js
+++ js/index.js
@@ -1,28 +1,24 @@
 const SCOPE_ACTIVITY_FITNESS_READONLY = "https://www.googleapis.com/auth/googlehealth.activity_and_fitness.readonly"
+const SCOPE_HEALTH_METRICS_READONLY = "https://www.googleapis.com/auth/googlehealth.health_metrics_and_measurements.readonly"
 
 const loadFrame = document.getElementById("load_frame")
 const loadLabel = loadFrame.querySelector("strong")
 
-function handleCredentialResponse(response) {
-	const responsePayload = decodeJwtResponse(response.credential)
+const stepsRadial = radialHelper(document.getElementById("steps_radial"))
+const distanceRadial = radialHelper(document.getElementById("distance_radial"))
+const activityRadial = radialHelper(document.getElementById("activity_radial"))
+const pulseRadial = radialHelper(document.getElementById("pulse_radial"))
 
-	console.log(response.credential)
-	console.log("ID: " + responsePayload.sub)
-	console.log("Full Name: " + responsePayload.name)
-	console.log("Given Name: " + responsePayload.given_name)
-	console.log("Family Name: " + responsePayload.family_name)
-	console.log("Image URL: " + responsePayload.picture)
-	console.log("Email: " + responsePayload.email)
-}
-
-function decodeJwtResponse(token) {
-	let base64Url = token.split(".")[1]
-	let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/")
-	let jsonPayload = decodeURIComponent(atob(base64).split("").map(function (c) {
-		return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
-	}).join(""))
-
-	return JSON.parse(jsonPayload)
+/**
+ * @param {HTMLElement} radial The radial element to control
+ */
+function radialHelper(radial) {
+	const bar = radial.querySelector(".radial-progress-bar")
+	const label = radial.querySelector("h1")
+	return (percent, text) => {
+		bar.style.setProperty("--value", percent * 100)
+		label.innerHTML = text
+	}
 }
 
 /**
@@ -72,7 +68,7 @@ function main() {
 	loadLabel.innerText = "Requesting Data..."
 	const batch = gapi.client.newBatch()
 	const resource = {
-		range: getDayInterval(new Date(), -7)
+		range: getDayInterval(new Date(), 0)
 	}
 
 	batch.add(gapi.client.health.users.dataTypes.dataPoints.dailyRollUp({
@@ -85,91 +81,126 @@ function main() {
 		resource
 	}), { id: "distance" })
 
-	batch.then((result) => {
-		if (!result.result) {
+	batch.add(gapi.client.health.users.dataTypes.dataPoints.dailyRollUp({
+		parent: "users/me/dataTypes/active-minutes",
+		resource
+	}), { id: "activity" })
+
+	batch.add(gapi.client.health.users.dataTypes.dataPoints.dailyRollUp({
+		parent: "users/me/dataTypes/heart-rate",
+		resource
+	}), { id: "heartrate" })
+
+	batch.then((response) => {
+		let data
+
+		const results = response.result
+		if (!results) {
 			loadLabel.innerText = "Failure"
 			return
 		}
 
-		const steps = result.result.steps
-		if (!steps) {
-			loadLabel.innerText = "Failure"
-			return
+		const steps = results.steps
+		if (data = steps?.result?.rollupDataPoints) {
+			if (data[0]) {
+				stepsRadial(0.5, data[0].steps.countSum)
+			} else {
+				stepsRadial(0, "N/A")
+			}
+		} else {
+			stepsRadial(0, "N/A")
 		}
 
-		if (!steps.result) {
-			loadLabel.innerText = "Failure"
-			return
+		const distance = results.distance
+		if (data = distance?.result?.rollupDataPoints) {
+			if (data[0]) {
+				const mm = parseInt(data[0].distance.millimetersSum) * 0.001
+				if (mm >= 1000) {
+					distanceRadial(0.5, (mm * 0.001).toFixed(2) + ` <small style="font-size: 1rem;">km</small>`)
+				} else {
+					distanceRadial(0.5, mm + ` <small style="font-size: 1rem;">m</small>`)
+				}
+			} else {
+				distanceRadial(0, "N/A")
+			}
+		} else {
+			distanceRadial(0, "N/A")
 		}
 
-		const distance = result.result.distance
-		if (!steps) {
-			loadLabel.innerText = "Failure"
-			return
+		const activity = results.activity
+		if (data = activity?.result?.rollupDataPoints) {
+
+		} else {
+			activityRadial(0, "N/A")
 		}
 
-		if (!steps.result) {
-			loadLabel.innerText = "Failure"
-			return
+		const heartrate = results.heartrate
+		if (data = heartrate?.result?.rollupDataPoints) {
+
+		} else {
+			pulseRadial(0, "N/A")
 		}
 
-		loadLabel.innerText = "Success"
-		console.log(steps.result, distance.result)
+		console.log(results)
+		loadFrame.remove()
 	}).catch((error) => {
 		console.error(error)
 	})
 }
 
-gapi.load("client", async () => {
-	const token = localStorage.getItem("access_token")
-	const exp = localStorage.getItem("exp")
+window.onload = () => {
+	gapi.load("client", async () => {
+		const token = localStorage.getItem("access_token")
+		const exp = localStorage.getItem("exp")
 
-	if (token && exp && exp > Date.now()) {
-		gapi.client.setToken({
-			access_token: token
-		})
+		if (token && exp && exp > Date.now()) {
+			gapi.client.setToken({
+				access_token: token
+			})
 
-		await gapi.client.load("https://health.googleapis.com/$discovery/rest?version=v4")
-		main()
-	} else {
-		const modal = new bootstrap.Modal('#signin_model')
-		const signinButton = document.getElementById("signin_button")
-		const tokenClient = google.accounts.oauth2.initTokenClient({
-			client_id: "630310444486-1qlt8f3u2n35rglmtjbuc6vkcstavihh.apps.googleusercontent.com",
-			scope: SCOPE_ACTIVITY_FITNESS_READONLY,
-			prompt: "",
-			callback: (tokenResponse) => {
-				if (!tokenResponse?.access_token) {
-					console.warn("No token response provided")
-					signinButton.disabled = false
-					signinButton.children[0].hidden = true
-					signinButton.children[1].innerText = "Sign In"
-					return
+			await gapi.client.load("https://health.googleapis.com/$discovery/rest?version=v4")
+			main()
+		} else {
+			const modal = new bootstrap.Modal('#signin_model')
+			const signinButton = document.getElementById("signin_button")
+			const tokenClient = google.accounts.oauth2.initTokenClient({
+				client_id: "630310444486-1qlt8f3u2n35rglmtjbuc6vkcstavihh.apps.googleusercontent.com",
+				scope: SCOPE_ACTIVITY_FITNESS_READONLY + " " + SCOPE_HEALTH_METRICS_READONLY,
+				prompt: "",
+				callback: (tokenResponse) => {
+					console.log(tokenResponse)
+					if (!tokenResponse?.access_token) {
+						console.warn("No token response provided")
+						signinButton.disabled = false
+						signinButton.children[0].hidden = true
+						signinButton.children[1].innerText = "Sign In"
+						return
+					}
+
+					if (!google.accounts.oauth2.hasGrantedAllScopes(tokenResponse, SCOPE_ACTIVITY_FITNESS_READONLY)) {
+						console.warn("Desired scopes have not been granted")
+						signinButton.disabled = false
+						signinButton.children[0].hidden = true
+						signinButton.children[1].innerText = "Sign In"
+						return
+					}
+
+					localStorage.setItem("access_token", tokenResponse.access_token)
+					localStorage.setItem("exp", Date.now() + tokenResponse.expires_in * 1000)
+					modal.hide()
+					main()
 				}
+			})
 
-				if (!google.accounts.oauth2.hasGrantedAllScopes(tokenResponse, SCOPE_ACTIVITY_FITNESS_READONLY)) {
-					console.warn("Desired scopes have not been granted")
-					signinButton.disabled = false
-					signinButton.children[0].hidden = true
-					signinButton.children[1].innerText = "Sign In"
-					return
-				}
-
-				localStorage.setItem("access_token", tokenResponse.access_token)
-				localStorage.setItem("exp", Date.now() + tokenResponse.expires_in * 1000)
-				modal.hide()
-				main()
+			signinButton.onclick = () => {
+				signinButton.disabled = true
+				signinButton.children[0].hidden = false
+				signinButton.children[1].innerText = "Signing In..."
+				tokenClient.requestAccessToken()
 			}
-		})
 
-		signinButton.onclick = () => {
-			signinButton.disabled = true
-			signinButton.children[0].hidden = false
-			signinButton.children[1].innerText = "Signing In..."
-			tokenClient.requestAccessToken()
+			gapi.client.load("https://health.googleapis.com/$discovery/rest?version=v4")
+			modal.show()
 		}
-
-		gapi.client.load("https://health.googleapis.com/$discovery/rest?version=v4")
-		modal.show()
-	}
-})
+	})
+}
